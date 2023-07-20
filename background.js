b0f3a1efe3f07da6436a9f16277b3b7e77379def
Rodrigo Jara <rodrigo.jara@andbank.com>
2023-07-20 18:28:16 +0200

    progressiong

--- background.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-if (chrome.action && chrome.action.onClicked) {
-    chrome.action.onClicked.addListener(function (tab) {
-        chrome.tabs.create({ url: "pop_v1.html" });
-    });
-}
-;
